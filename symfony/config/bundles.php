c1d0b4ff9c58642e95a04b5e42745c4b9893a2f4
Sergej Karavajnij <basyanya@gmail.com>
2020-10-22 00:14:06 +0300

    Add required packages

--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
